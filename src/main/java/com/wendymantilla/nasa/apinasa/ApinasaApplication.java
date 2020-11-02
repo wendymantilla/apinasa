1fa1b53140d537cf6f24ba67e2fd593c975d6930
Wendy Mantilla <wmantilla113@gmail.com>
2020-11-02 15:50:42 -0500

    first commit master

---
+++ src/main/java/com/wendymantilla/nasa/apinasa/ApinasaApplication.java
@@ -0,0 +1,13 @@
+package com.wendymantilla.nasa.apinasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApinasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApinasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wendymantilla/nasa/apinasa/ApinasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendymantilla.nasa.apinasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApinasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
